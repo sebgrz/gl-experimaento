94ba6aac5f42be942633f4b895e24ed8e68b9182
Seba G <sebag.git@gmail.com>
2022-09-08 22:09:17 +0200

    Use SDL events loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_error.h>
+#include <SDL2/SDL_events.h>
 #include <SDL2/SDL_timer.h>
 #include <SDL2/SDL_video.h>
 #include <iostream>
@@ -11,7 +12,22 @@ int main() {
     cout << SDL_GetError() << endl;
   }
 
-  SDL_CreateWindow("test", 0, 0, 800, 600, 0);
-  while(1);
+  auto window = SDL_CreateWindow("test", 0, 0, 800, 600, 0);
+  auto close = false;
+  while(!close) {
+    auto event = SDL_Event();
+    while (SDL_PollEvent(&event)) {
+      switch (event.type) {
+        case SDL_QUIT:
+          close = true;
+          break;
+        default:
+          break;
+      }
+    }
+  }
+
+  SDL_DestroyWindow(window);
+  SDL_Quit();
   return 0;
 }
